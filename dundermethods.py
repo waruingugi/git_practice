d941e0cfa44341eae367ca6ad2967de9c6505b1f
waruingugi <waruingugientp@gmail.com>
2022-12-05 10:19:02 +0300

    Practice dunder method

---
+++ dundermethods.py
@@ -0,0 +1,32 @@
+# Dunder methods refers to methods that start Double UNDERscore e.g __str__
+# Every class has access to a special group of this methods.
+# You can perform operator overloading
+
+class Employee():
+    new_id = 1
+    def __init__(self):
+        self.id = Employee.new_id
+        Employee.new_id += 1
+
+
+class Meeting():
+    def __init__(self):
+        self.attendees = []
+
+    def __add__(self, employee):
+        print(f"ID {employee.id} added.")
+        self.attendees.append(employee)
+
+    def __len__(self):
+        return len(self.attendees)
+
+
+e1 = Employee()
+e2 = Employee()
+e3 = Employee()
+m1 = Meeting()
+m1 + e1
+m1 + e2
+m1 + e3
+print(len(m1))
+print(m1)
