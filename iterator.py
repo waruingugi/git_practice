44f7f647781708b4d4649b92e370811e6ad247cf
waruingugi <waruingugientp@gmail.com>
2022-12-08 20:32:16 +0300

    Practice customer iterators

---
+++ iterator.py
@@ -0,0 +1,20 @@
+# Custom iterator
+
+class CustomerCounter:
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        self.count += 1
+
+        if self.count > 100:
+            raise StopIteration
+        return self.count
+
+
+customer_counter = CustomerCounter()
+
+for customer in customer_counter:
+    print(customer)
+
